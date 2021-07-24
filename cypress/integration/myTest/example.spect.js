4cad4ec8275e5bdb38d9bb2b03c2779a74b0f164
Anthony Amaro <anthonyamaro5555@gmail.com>
2021-07-24 16:27:56 -0700

    feat: finsih cypress tutorial

--- cypress/integration/myTest/example.spect.js
+++ cypress/integration/myTest/example.spect.js
@@ -10,6 +10,13 @@ describe("My first real test", () => {
    it("visits the kitchen sink", () => {
       cy.visit("https://example.cypress.io");
 
+      cy.pause();
+
       cy.contains("type").click();
+
+      cy.url().should("include", "/commands/actions");
+      cy.get(".action-email")
+         .type("fake@email.com")
+         .should("have.value", "fake@email.com");
    });
 });
