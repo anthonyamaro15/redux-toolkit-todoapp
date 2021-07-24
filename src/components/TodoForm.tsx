748915b57322cd2a5c3a2eecc026ffca4d79c93b
Anthony Amaro <anthonyamaro5555@gmail.com>
2021-07-24 16:33:40 -0700

    refactor: add basic css

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,27 +1,34 @@
-import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { Todos } from '../interfaces';
-import { newTodo } from '../redux/slices/todoSlice';
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { Todos } from "../interfaces";
+import { newTodo } from "../redux/slices/todoSlice";
 
 const TodoForm = () => {
    const dispatch = useDispatch();
    const { register, handleSubmit, reset } = useForm();
-   
+
    const addTodo = (value: Todos) => {
-      dispatch(newTodo({...value, isCompleted: false, id: new Date().toISOString() }));
+      dispatch(
+         newTodo({
+            ...value,
+            isCompleted: false,
+            id: new Date().toISOString(),
+         }),
+      );
       reset();
-   }
+   };
 
    return (
       <div className="addTodo-form">
          <form onSubmit={handleSubmit(addTodo)}>
             <label htmlFor="todo">
+               add todo
                <input type="text" name="todo" id="todo" ref={register} />
             </label>
             <button type="submit">add</button>
          </form>
       </div>
-   )
-}
+   );
+};
 
 export default TodoForm;
